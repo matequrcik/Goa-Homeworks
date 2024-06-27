382def71536e6af54ddda03a096b83d48596b31e
mate <matequrcik@gmail.com>
2024-06-27 11:01:12 +0400

    commit

---
+++ day 38/homework/hw.py
@@ -0,0 +1,5 @@
+goa_group_7=["Mate qurcikidze","Lazare iakobadze","Tazo editerashvili","Aleqsandre chanqseliani","Andria giorgadze","Merabi gaganidze","Merab chuxrukidze"]
+print(goa_group_7[0:5])
+print(goa_group_7[0:3])
+print(goa_group_7[3:6])
+print(goa_group_7[:])
