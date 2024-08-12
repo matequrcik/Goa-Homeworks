4ab766b9fb519d12fb81e96f319ba569e93510ed
mate <matequrcik@gmail.com>
2024-08-12 17:35:44 +0400

    commit

---
+++ day 54/homework/task2.py
@@ -0,0 +1,12 @@
+"""გადაიმეორეთ python-ში ყველა ნასწავლი მასალა, გააკეთეთ 3-3 მაგალითი"""
+
+
+for i in range(20,30):
+    print(i)
+
+for i in range(13,58,2):
+    print(i)
+
+windows=["8","8.1","10","11"]
+for i in range(len(windows)):
+    print(windows[i])
